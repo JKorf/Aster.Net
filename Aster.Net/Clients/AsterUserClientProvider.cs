3c07f1f19ff3fda8c8588bf5076ceb60eef1bde5
Jkorf <jankorf91@gmail.com>
2026-02-02 09:34:54 +0100

    Added check for disposed client in UserClientProvider

--- Aster.Net/Clients/AsterUserClientProvider.cs
+++ Aster.Net/Clients/AsterUserClientProvider.cs
@@ -64,7 +64,7 @@ namespace Aster.Net.Clients
         /// <inheritdoc />
         public IAsterRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, AsterEnvironment? environment = null)
         {
-            if (!_restClients.TryGetValue(userIdentifier, out var client))
+            if (!_restClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateRestClient(userIdentifier, credentials, environment);
 
             return client;
@@ -73,7 +73,7 @@ namespace Aster.Net.Clients
         /// <inheritdoc />
         public IAsterSocketClient GetSocketClient(string userIdentifier, ApiCredentials? credentials = null, AsterEnvironment? environment = null)
         {
-            if (!_socketClients.TryGetValue(userIdentifier, out var client))
+            if (!_socketClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateSocketClient(userIdentifier, credentials, environment);
 
             return client;
