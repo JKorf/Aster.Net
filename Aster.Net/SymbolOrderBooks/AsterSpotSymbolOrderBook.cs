8a4632d31494cdc18bb2401bcc3b4d4361251127
Jkorf <jankorf91@gmail.com>
2026-02-16 10:22:58 +0100

    Fixed SymbolOrderBook websocket subscription not getting closed if when waiting for initial data times out

--- Aster.Net/SymbolOrderBooks/AsterSpotSymbolOrderBook.cs
+++ Aster.Net/SymbolOrderBooks/AsterSpotSymbolOrderBook.cs
@@ -104,6 +104,9 @@ namespace Aster.Net.SymbolOrderBooks
             else
             {
                 var setResult = await WaitForSetOrderBookAsync(_initialDataTimeout, ct).ConfigureAwait(false);
+                if (!setResult)
+                    await subResult.Data.CloseAsync().ConfigureAwait(false);
+
                 return setResult ? subResult : new CallResult<UpdateSubscription>(setResult.Error!);
             }
 
