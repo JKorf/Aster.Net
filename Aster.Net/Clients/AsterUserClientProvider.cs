c7390124054ecfa91e83b8d0a1777c250807fc48
Jkorf <jankorf91@gmail.com>
2026-02-24 13:15:37 +0100

    Updated HttpClient registration logic

--- Aster.Net/Clients/AsterUserClientProvider.cs
+++ Aster.Net/Clients/AsterUserClientProvider.cs
@@ -42,6 +42,9 @@ namespace Aster.Net.Clients
             IOptions<AsterSocketOptions> socketOptions)
         {
             _httpClient = httpClient ?? new HttpClient();
+            _httpClient.Timeout = restOptions.Value.RequestTimeout;
+            
+
             _loggerFactory = loggerFactory;
             _restOptions = restOptions;
             _socketOptions = socketOptions;
