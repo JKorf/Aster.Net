ad3329e0c705740fa56f1ea0647d4a24b6da36bb
Jkorf <jankorf91@gmail.com>
2026-04-09 09:17:45 +0200

    Updated restClient.FuturesV3Api.Account.GetAccountInfoAsync path from /account to /accountWithJoinMargin

--- Aster.Net/Clients/FuturesV3Api/AsterRestClientFuturesV3ApiAccount.cs
+++ Aster.Net/Clients/FuturesV3Api/AsterRestClientFuturesV3ApiAccount.cs
@@ -140,7 +140,7 @@ namespace Aster.Net.Clients.FuturesV3Api
             var parameters = new ParameterCollection();
             parameters.AddOptionalParameter("recvWindow", receiveWindow?.ToString(CultureInfo.InvariantCulture) ?? _baseClient.ClientOptions.ReceiveWindow.TotalMilliseconds.ToString(CultureInfo.InvariantCulture));
 
-            var request = _definitions.GetOrCreate(HttpMethod.Get, "/fapi/v3/account", AsterExchange.RateLimiter.RestIp, 5, true);
+            var request = _definitions.GetOrCreate(HttpMethod.Get, "/fapi/v3/accountWithJoinMargin", AsterExchange.RateLimiter.RestIp, 5, true);
             return await _baseClient.SendAsync<AsterAccountInfo>(request, parameters, ct).ConfigureAwait(false);
         }
 
