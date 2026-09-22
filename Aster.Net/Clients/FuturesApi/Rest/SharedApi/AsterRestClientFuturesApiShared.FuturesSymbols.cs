7dbfb7195c3bca699d942822fe6ef260c9f3eb2c
Jkorf <jankorf91@gmail.com>
2026-09-22 15:36:57 +0200

    wip

--- Aster.Net/Clients/FuturesApi/Rest/SharedApi/AsterRestClientFuturesApiShared.FuturesSymbols.cs
+++ Aster.Net/Clients/FuturesApi/Rest/SharedApi/AsterRestClientFuturesApiShared.FuturesSymbols.cs
@@ -23,7 +23,7 @@ namespace Aster.Net.Clients.FuturesApi
 
         public GetFuturesSymbolsOptions GetFuturesSymbolsOptions { get; } 
             = new GetFuturesSymbolsOptions(_exchangeName, false);
-        async Task<ICallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetFuturesSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public async Task<HttpResult<SharedFuturesSymbol[]>> GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
